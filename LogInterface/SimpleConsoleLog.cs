88d111757239c8996a68742d3819badf485da9d1
eveil <erick@erickveil.net>
2016-03-30 15:06:29 -0700

    First commit - just a debug

---
+++ LogInterface/SimpleConsoleLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogInterface
+{
+    public class SimpleConsoleLog : ILog
+    {
+        public void LogDebug(String msg)
+        {
+            Console.WriteLine("DEBUG | "+msg);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+LogInterface
+
+Provides an interface for different types of logging.
+
+I like to use the same logging interface in everything. Then I can swap out log types on the fly. System logs? Logs to file? Logs to the console? Unity console logs? There are a lot of logging options, and I don't like commitment.
